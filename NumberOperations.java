ab0253bec6ef89678a8a8c7efc225ae29f789784
Roohi Fathima[192121112] <112243951+roohiii@users.noreply.github.com>
2023-10-19 21:00:06 +0530

    Create NumberOperations.java

---
+++ NumberOperations.java
@@ -0,0 +1,9 @@
+public class NumberOperations {
+    public static double calculateSquare(double number) {
+        return number * number;
+    }
+
+    public static double calculateCube(double number) {
+        return number * number * number;
+    }
+}

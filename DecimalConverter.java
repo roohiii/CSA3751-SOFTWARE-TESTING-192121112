dcc445b4f97d5bf9ccbb7a9560f272adb9b2b9e6
Roohi Fathima[192121112] <112243951+roohiii@users.noreply.github.com>
2023-10-19 20:53:08 +0530

    Create DecimalConverter.java

---
+++ DecimalConverter.java
@@ -0,0 +1,9 @@
+public class DecimalConverter {
+    public static String decimalToBinary(int decimalNum) {
+        return Integer.toBinaryString(decimalNum);
+    }
+
+    public static String decimalToOctal(int decimalNum) {
+        return Integer.toOctalString(decimalNum);
+    }
+}
